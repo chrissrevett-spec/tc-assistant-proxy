342f18e48215232bd4c827c5faa0dc944edd400b
chrissrevett-spec <chrissrevett@gmail.com>
2025-09-07 19:02:25 +0100

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,33 +1,42 @@
-// Simple CORS helper for Vercel Node functions
-const ORIGIN = "https://www.talkingcare.uk";
+export function getAllowedOrigin() {
+  return process.env.ALLOWED_ORIGIN || "*";
+}
 
-function corsHeaders(extra = {}) {
+export function commonCorsHeaders(extra = {}) {
   return {
-    "Access-Control-Allow-Origin": ORIGIN,
+    "Access-Control-Allow-Origin": getAllowedOrigin(),
     "Vary": "Origin",
-    "Access-Control-Allow-Methods": "POST, OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, x-vercel-protection-bypass, Accept",
     "Access-Control-Max-Age": "86400",
-    ...extra,
+    ...extra
   };
 }
 
-function send(res, status, body, extra = {}) {
-  const headers = corsHeaders(extra);
-  res.statusCode = status;
-  for (const [k, v] of Object.entries(headers)) res.setHeader(k, v);
-  if (typeof body === "string") {
-    res.setHeader("Content-Type", extra["Content-Type"] || "text/plain; charset=utf-8");
-    res.end(body);
-  } else {
-    res.setHeader("Content-Type", extra["Content-Type"] || "application/json; charset=utf-8");
-    res.end(JSON.stringify(body));
-  }
+export function okJson(obj, init = {}) {
+  return new Response(JSON.stringify(obj), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      ...commonCorsHeaders(),
+      ...(init.headers || {})
+    },
+  });
 }
 
-function ok(res, body, extra = {}) { send(res, 200, body, extra); }
-function noContent(res)           { send(res, 204, "", {}); }
-function bad(res, msg)            { send(res, 400, { ok:false, error: msg }); }
-function fail(res, msg)           { send(res, 500, { ok:false, error: msg }); }
+export function errJson(status, obj) {
+  return new Response(JSON.stringify(obj), {
+    status,
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      ...commonCorsHeaders()
+    },
+  });
+}
 
-module.exports = { corsHeaders, send, ok, noContent, bad, fail, ORIGIN };
+export function handleOptions() {
+  return new Response(null, {
+    status: 204,
+    headers: commonCorsHeaders(),
+  });
+}
